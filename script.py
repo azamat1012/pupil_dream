7d82b5b170d4536719eb49973407f07553c1ff9f
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2024-11-10 18:21:53 +0500

    Update script.py

--- script.py
+++ script.py
@@ -14,24 +14,16 @@ def get_student_by_name(name):
 def improve_grades(name):
     """Функция для массового улучшения плохих оценок."""
     student = get_student_by_name(name)
-    if isinstance(student, str):
-        return student  
     updated_count = Mark.objects.filter(schoolkid=student, points__lt=4).update(points=4)
 
 def delete_all_chastisements(name):
     """Функция для удаления замечаний."""
     student = get_student_by_name(name)
-    if isinstance(student, str):
-        return student 
-    
     Chastisement.objects.filter(schoolkid=student).delete()
 
 def create_commendation(student_name, subject_title):
     """Функция для создания похвал."""
     student = get_student_by_name(student_name)
-    if isinstance(student, str):
-        return student 
-    
     try:
         subject = Subject.objects.get(
             title=subject_title, year_of_study=student.year_of_study
